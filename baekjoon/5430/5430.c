ff4624bc451166e8b78cf809d4df1db0968fb6fb
AtraFelis <cotjs0321@naver.com>
2024-03-25 18:56:41 +0900

    0328 출석인정

--- baekjoon/5430/5430.c
+++ baekjoon/5430/5430.c
@@ -3,88 +3,73 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void reverse(int *arr, int *len);
-bool delete(int *arr, int *len);
-
 int main() {
     int t;
 
     scanf("%d", &t);
-    getchar(); // 엔터 제거
-
-    int *arr;
 
     for (int i = 0; i < t; i++)
     {
         bool error_check = false;
-        char command[100000];
+        char command[100001];
         
+        while(getchar() != '\n'); // 버퍼 제거
         gets(command);
 
         int n;
         scanf("%d", &n);
         getchar(); // 엔터 제거
-        arr = (int*)malloc(sizeof(int)*n);
+
+        int arr[100001];
+
+        int k=0;
+        while(getchar() != ']' && n != 0)
+            scanf("%d", &arr[k++]);
         
-        getchar(); // '[' 제거
-        for (int k = 0; k < n; k++)
-        {
-            scanf("%d", &arr[k]);
-            getchar(); // 컴마 제거
-        }
-        getchar(); // ']' 제거
+        if(k != n) error_check = true;
 
-        for (int k = 0; k < strlen(command); k++)
+        bool reverse = false;
+        int front = 0, rear = n;
+
+        int len = strlen(command);
+        for (int k = 0; k < len; k++)
         {
-            switch (command[k])
-            //RR : 변화 없음
-            //RD : 맨 마지막 없애고 R -> 연산 더 빠름
-            {
-            case 'R':                
-                reverse(arr, &n);
-                break;
-            
-            case 'D':
-                error_check = !(delete(arr, &n));
-                break;
+            if(command[k] == 'R')
+                reverse = !reverse;
+            else { // command[i] == 'D'
+                if(front == rear) {
+                    error_check = true;
+                    break;
+                }                
+                if(reverse) {    
+                    rear--;
+                } else {
+                    front++;
+                }
             }
-
-            if(error_check) break;
         }
 
         if(error_check)
             printf("error\n");
         else {
-            printf("[");
-            for (int i = 0; i < n-1; i++)
-                printf("%d,", arr[i]);
-            printf("%d]\n", arr[n-1]);
+            if(reverse){
+                printf("[");
+                for (int j = rear-1; j > front; j--)
+                    printf("%d,", arr[j]);
+                if(front < rear)
+                    printf("%d", arr[front]);
+                printf("]\n");
+            }
+            else {
+                printf("[");
+                for (int j = front; j < rear-1; j++)
+                    printf("%d,", arr[j]);
+                if(front < rear)
+                    printf("%d", arr[rear-1]);
+                printf("]\n");
+            }
         }
-
-        free(arr);
     }
     
     return 0;
-}
-
-void reverse(int *arr, int *len) {
-    int tmp;
-    int idx = *len-1;
-    for (int i = 0; i < *len / 2; i++) {
-        tmp = arr[i];
-        arr[i] = arr[idx];
-        arr[idx--] = tmp;
-    }
-}
-
-bool delete(int *arr, int *len) {
-    if(*len != 0) {
-        for (int i = 1; i < *len; i++)
-            arr[i-1] = arr[i];
-        (*len)--;
-
-        return true;
-    }
-
-    return false;
 }
