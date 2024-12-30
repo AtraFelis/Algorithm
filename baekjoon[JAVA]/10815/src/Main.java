43552819a2dd3e8889434c41b32875052fbffdd9
AtraFelis <cotjs0321@naver.com>
2024-12-30 23:06:10 +0900

    10815 풀이 추가

--- baekjoon[JAVA]/10815/src/Main.java
+++ baekjoon[JAVA]/10815/src/Main.java
@@ -1,11 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    // use HashSet
+/*    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n, m;
@@ -22,10 +24,37 @@ public class Main {
         m = Integer.parseInt(br.readLine());
         stk = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            sb.append(set.contains(Integer.parseInt(stk.nextToken())) ? "1" : "0")
-                    .append(' ');
+            sb.append(set.contains(Integer.parseInt(stk.nextToken())) ? "1" : "0").append(' ');
         }
 
+        System.out.println(sb);
+    }*/
+
+
+    // Use Binary Search
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n, m;
+        StringTokenizer stk;
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+
+        stk = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        // 배열 정렬
+        Arrays.sort(nums);
+
+        m = Integer.parseInt(br.readLine());
+        stk = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int idx = Arrays.binarySearch(nums, Integer.parseInt(stk.nextToken()));
+            sb.append(idx >= 0 ? "1 " : "0 ");
+        }
         System.out.println(sb);
     }
 }
